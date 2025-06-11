521beb9cd5062097a6acdf2fe3e096fc88d9d489
David Franco <guerrdavid.pichardo@gmail.com>
2025-06-11 16:56:20 -0500

    Finalizing

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
 $(document).ready(function () {
     // write your code here
-
+    $.getJSON("data.json", function(data) {
+            let tableBody = $("#usertable-tbody");
+            $.each(data, function(index, user) {
+                let [lat, lon] = user.location;
+                let mapsLink = `https://www.google.com/maps?q=${lat},${lon}`;
+                let row = `<tr>
+                    <td>${user.name}</td>
+                    <td>${user.description}</td>
+                    <td><a href="${mapsLink}" target="_blank">Open In Maps</a></td>
+                </tr>`;
+                tableBody.append(row);
+            });
+        });
 });
